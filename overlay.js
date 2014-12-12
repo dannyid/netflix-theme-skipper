16cd74098aca2d48835063c3c773ce9bba1cd65a
dannyid <danielshekhtman@gmail.com>
2014-12-12 04:24:08 -0500

    adding internal divs to overlay

--- overlay.js
+++ overlay.js
@@ -1,7 +1,10 @@
 window.$jq = jQuery;
 
-$jq('body').append('<div id="nfs-overlay"></div>');
+$jq('body').append('<div id="nts-overlay"></div>');
 
-var $overlay = $jq('div#nfs-overlay');
+var $overlay = $jq('div#nts-overlay');
+
+$overlay.append('<div id="nts-mini-close"></div>')
+$overlay.append('<div id="nts-mini-overlay">N</div>')
 
 console.log($overlay);
