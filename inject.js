4169df939b730d7fd6ed5ff15df11ea535b29d46
dannyid <danielshekhtman@gmail.com>
2014-12-12 01:59:57 -0500

    adding overlay script

--- inject.js
+++ inject.js
@@ -1,9 +1,14 @@
-var s = document.createElement('script');
-
-s.src = chrome.extension.getURL('script.js');
-
-s.onload = function() {
+var themeSkipperScript = document.createElement('script');
+themeSkipperScript.src = chrome.extension.getURL('script.js');
+themeSkipperScript.onload = function() {
     this.parentNode.removeChild(this);
 };
+(document.head||document.documentElement).appendChild(themeSkipperScript);
 
-(document.head||document.documentElement).appendChild(s);
+var overlay = document.createElement('script');
+overlay.src = chrome.extension.getURL('overlay.js');
+overlay.onload = function() {
+    this.parentNode.removeChild(this);
+};
+(document.head||document.documentElement).appendChild(overlay);
+
