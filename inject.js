a22d5874177a5cb705b6b6e93f53add32ec0f177
dannyid <danielshekhtman@gmail.com>
2014-12-15 01:25:06 -0500

    removing overlay.js because i moved it into the main script

--- inject.js
+++ inject.js
@@ -4,10 +4,3 @@ themeSkipperScript.onload = function() {
     this.parentNode.removeChild(this);
 };
 (document.head||document.documentElement).appendChild(themeSkipperScript);
-
-var overlayScript = document.createElement('script');
-overlayScript.src = chrome.extension.getURL('overlay.js');
-overlayScript.onload = function() {
-    this.parentNode.removeChild(this);
-};
-(document.head||document.documentElement).appendChild(overlayScript);
